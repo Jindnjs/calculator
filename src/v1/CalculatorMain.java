62169c39fe221a7aa21ae08f6b200ae6e6a6725c
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2025-04-16 19:22:54 +0900

    feat : 무한 반복 계산기능 추가

--- src/v1/CalculatorMain.java
+++ src/v1/CalculatorMain.java
@@ -8,52 +8,63 @@ public class CalculatorMain {
         /*스캐너 객체 선언*/
         Scanner scanner = new Scanner(System.in);
 
-        /*첫번째 숫자 입력*/
-        int firstNum = inputInt(1, scanner);
-        /*두번쨰 숫자 입력*/
-        int secondNum = inputInt(2, scanner);
+        /*무한 반복*/
+        while (true) {
+            /*첫번째 숫자 입력*/
+            int firstNum = inputInt(1, scanner);
+            /*두번쨰 숫자 입력*/
+            int secondNum = inputInt(2, scanner);
 
-        /*연산기호 검사에 쓰일 배열*/
-        char [] operators = {'+', '-', '*', '/'};
-        char operator;
-        /*연산 기호 입력*/
-        /*연산기호가 + - * / 가 아니면 다시 입력받기*/
-        while(true) {
-            /*while 탈출 변수*/
-            boolean flag = false;
+            /*연산기호 검사에 쓰일 배열*/
+            char[] operators = {'+', '-', '*', '/'};
+            char operator;
+            /*연산 기호 입력*/
+            /*연산기호가 + - * / 가 아니면 다시 입력받기*/
+            while (true) {
+                /*while 탈출 변수*/
+                boolean flag = false;
 
-            /*연산자 입력*/
-            System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
-            operator = scanner.next().charAt(0);
-            for (char a : operators) {
-                if (operator == a) {
-                    flag = true;
+                /*연산자 입력*/
+                System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
+                operator = scanner.next().charAt(0);
+                scanner.nextLine();
+
+                for (char a : operators) {
+                    if (operator == a) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag) {
                     break;
                 }
+                System.out.println("(+, -, *, /)만 입력하세요.");
             }
-            if (flag) {break;}
-            System.out.println("(+, -, *, /)만 입력하세요.");
+
+            /*연산 수행 및 출력*/
+            switch (operator) {
+                case '+':
+                    System.out.println("result = " + (firstNum + secondNum));
+                    break;
+                case '-':
+                    System.out.println("result = " + (firstNum - secondNum));
+                    break;
+                case '*':
+                    System.out.println("result = " + (firstNum * secondNum));
+                    break;
+                case '/':
+                    try {
+                        System.out.println("result = " + (firstNum / secondNum));
+                    } catch (ArithmeticException e) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    }
+                    break;
+            }
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료 / 아무거나 입력) : ");
+            String input = scanner.nextLine();
+            if (input.equals("exit")) {break;}
         }
 
-        /*연산 수행 및 출력*/
-        switch(operator) {
-            case '+':
-                System.out.println("result = " + (firstNum + secondNum));
-                break;
-            case '-':
-                System.out.println("result = " + (firstNum - secondNum));
-                break;
-            case '*':
-                System.out.println("result = " + (firstNum * secondNum));
-                break;
-            case '/':
-                try {
-                    System.out.println("result = " + (firstNum / secondNum));
-                }catch (ArithmeticException e) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                }
-                break;
-        }
     }
 
     /*
