ab3c6939ad0ea627409e2ba572c54ec131d8586e
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2025-04-16 19:29:05 +0900

    fix : 숫자 입력에 문자 입력시 InputMismatchException 수정

--- src/v1/CalculatorMain.java
+++ src/v1/CalculatorMain.java
@@ -27,6 +27,7 @@ public class CalculatorMain {
                 /*연산자 입력*/
                 System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
                 operator = scanner.next().charAt(0);
+                /*버퍼 비우기*/
                 scanner.nextLine();
 
                 for (char a : operators) {
@@ -62,6 +63,7 @@ public class CalculatorMain {
             }
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료 / 아무거나 입력) : ");
             String input = scanner.nextLine();
+            /*입력이 exit이면 while 무한반복문 탈출*/
             if (input.equals("exit")) {break;}
         }
 
@@ -75,7 +77,23 @@ public class CalculatorMain {
     public static int inputInt(int sequence, Scanner scanner) {
         while (true) {
             System.out.print(sequence + "번째 숫자 입력 : ");
-            int inputNum = scanner.nextInt();
+
+            /*입력받은 값이 숫자인지 먼저 확인*/
+            /*문자 입력을 받고, parseInt로 숫자 여부를 검사*/
+            String inputValue = scanner.nextLine();
+            int inputNum;
+            try{
+                /*
+                * 만약 숫자입력이면, 오류없이 InputNum에 값이 int로 변환
+                * 문자입력이면, 문자 입력이므로, 다시 입력받기
+                */
+                inputNum = Integer.parseInt(inputValue);
+            }
+            catch (NumberFormatException e) {
+                System.out.println("양의정수(0포함)를 입력하세요");
+                continue;
+            }
+            /*입력 받은 숫자가 양수인지 확인*/
             if (inputNum < 0) {
                 System.out.println("양의정수(0포함)를 입력하세요");
                 continue;
